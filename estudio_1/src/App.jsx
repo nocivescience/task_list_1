6fcfdd18008102d707dacd3f118f0b03a93f2f72
noceive <nocivescience@gmail.com>
2022-12-26 18:04:41 -0300

    terminado

--- estudio_1/src/App.jsx
+++ estudio_1/src/App.jsx
@@ -1,39 +1,45 @@
-import { useState,useEffect } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./componentes/Home";
-import {Navbar} from './componentes/Navbar';
-import {TaskList} from './componentes/TaskList';
+import { Navbar } from "./componentes/Navbar";
+import { TaskList } from "./componentes/TaskList";
 import { Contacts } from "./componentes/Contacts";
-import {Products} from './componentes/Products';
+import { Products } from "./componentes/Products";
 import { Formulario } from "./componentes/Fortmulario";
-import {data} from './actividades'
+import { data } from "./actividades";
 
 function App() {
   const [tasks, setTasks] = useState([]);
-    useEffect(()=>{
-        setTasks(data)
-    },[])
-    function createTask(taskTitle){
-      setTasks([...tasks,{
-        title:taskTitle,
+  useEffect(() => {
+    setTasks(data);
+  }, []);
+  function createTask(taskTitle) {
+    setTasks([
+      ...tasks,
+      {
+        title: taskTitle,
         id: tasks.length,
-        description:'que mirai'
-      }])
-    }
+        description: "que mirai",
+      },
+    ]);
+  }
   return (
     <>
       <BrowserRouter>
-        <Navbar/>
+        <Navbar />
         <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/tasks' element={<TaskList tasks={tasks}/>}/>
-          <Route path='/contacts' element={<Contacts/>}/>
-          <Route path='/products' element={<Products/>}/>
-          <Route path='/formulario' element={<Formulario createTask={createTask} />}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/tasks" element={<TaskList tasks={tasks} />} />
+          <Route path="/contacts" element={<Contacts />} />
+          <Route path="/products" element={<Products />} />
+          <Route
+            path="/formulario"
+            element={<Formulario createTask={createTask} />}
+          />
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
